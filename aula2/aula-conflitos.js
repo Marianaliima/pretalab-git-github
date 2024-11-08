a3ede2a3a8db5d762744316b99cf36408389ae25
dayanecarla72@gmail.com <dayanecarla72@gmail.com>
2024-11-08 20:29:26 -0300

    alterações de dayane

--- aula2/aula-conflitos.js
+++ aula2/aula-conflitos.js
@@ -1,11 +1,6 @@
 const pessoa = {
-<<<<<<< HEAD
     nome: "dayane",
     idade: "31",
     apelido: "day"
 }
-=======
-    nome: "mariana",
-    idade: 15
-}
->>>>>>> d8a51d5d18c48f681a77c47f3f9c1b3439fbe7cc
+
